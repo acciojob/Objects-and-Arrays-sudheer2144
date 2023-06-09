0f0fe26b26e58472eab553b1289d687dc64dc939
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-09 08:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,19 +5,15 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-
 const team = players;
 
-const team1 = players.map(getValues(players));
+const team1 = players.map(getValues);
 
 function getValues(element){
     return element;
 }
 
 const cap1=person;
-
 
 
 window.players = players;
